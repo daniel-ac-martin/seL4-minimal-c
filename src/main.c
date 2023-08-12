5557700bd0484afd1eca1403fb5b4151e0d1a0f8
Daniel A.C. Martin <github@daniel-martin.co.uk>
2023-08-12 22:23:26 +0100

    Output more to debug and supend

--- src/main.c
+++ src/main.c
@@ -2,5 +2,19 @@
 
 int main(int argc, char *argv[]) {
 	seL4_DebugPutChar('H');
-}
+	seL4_DebugPutChar('e');
+	seL4_DebugPutChar('l');
+	seL4_DebugPutChar('l');
+	seL4_DebugPutChar('o');
+	seL4_DebugPutChar(',');
+	seL4_DebugPutChar(' ');
+	seL4_DebugPutChar('W');
+	seL4_DebugPutChar('o');
+	seL4_DebugPutChar('r');
+	seL4_DebugPutChar('l');
+	seL4_DebugPutChar('d');
+	seL4_DebugPutChar('!');
+	seL4_DebugPutChar('\n');
 
+  seL4_TCB_Suspend(seL4_CapInitThreadTCB);
+}
